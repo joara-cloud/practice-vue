6c0cccc38099976a7ef9400981ec99a88b8b2ce0
ara <joarario92@gmail.com>
2020-09-19 23:43:46 +0900

    Revert "practice route"
    This reverts commit 1ee878335fd8002fe09e997dedfcb7778447df4d.
    
    :wq
    deleted:    src/routes/index.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue';
-import {router} from './routes/index.js';
-
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  router
 }).$mount('#app')
