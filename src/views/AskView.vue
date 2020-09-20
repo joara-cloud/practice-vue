f92ab19e65d66b7b4735733473c763fb3a0a0ab0
ara <joarario92@gmail.com>
2020-09-20 13:13:26 +0900

    Promise

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -17,7 +17,7 @@ export default {
   },
   created() {
     fetchAskList() // 여기에서 return new Promise() 가 되기 때문에 then으로 연결 가능!!
-      .then(res => this.asks = res.data)
+      .then(res => this.asks = res.data) // 화살표 함수를 쓰게 되면 
       .catch(err => console.log(err));
   }
 }
